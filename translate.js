143d4ca0aa6c0cf818fc178cdf5178a3e82ffca7
kaito-kubota <58417159+kaito-kubota@users.noreply.github.com>
2026-07-20 18:39:27 +0900

    Update translate.js

--- translate.js
+++ translate.js
@@ -15,8 +15,14 @@ const dictionary = {
     "Time to Juvenile:": "少年期までの時間:",
     "Time to Adult:": "成体までの時間:",
     "Food to Juvenile (E):": "少年期までに食べる餌の量:",
-    "Food to Adult (E):": "成体までに食べる餌の量"
-    "Food Rate (E):": "食費率:"
+    "Food to Adult (E):": "成体までに食べる餌の量",
+    "Food Rate (E):": "食費率:",
+    "Food per period (E):": "期間当たりの食料:",
+    "Baby Phase": "幼年期",
+    "Current Buffer:": "最大まで餌を入れてどれくらい生きるか:",
+    "Food to Fill:": "持てる餌の量",
+    "Hand Feed For:": "手動餌やりが必要な時間:",
+    "Food to Finish:": ""
 };
 
 function translatePage() {
