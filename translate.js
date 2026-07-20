4c983fba121f24284a4ef80b09ea647bcced2c5f
kaito-kubota <58417159+kaito-kubota@users.noreply.github.com>
2026-07-20 17:33:01 +0900

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,1 +1,16 @@
-console.log("translate.js loaded");
+const dictionary = {
+    "Creature Details": "生物情報",
+};
+
+function translatePage() {
+    document.querySelectorAll("*").forEach(element => {
+        const text = element.textContent.trim();
+
+        if (dictionary[text]) {
+            element.textContent = dictionary[text];
+        }
+    });
+}
+
+// 0.5秒ごとに翻訳
+setInterval(translatePage, 500);
